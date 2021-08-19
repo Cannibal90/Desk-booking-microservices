99c6c5117d2b386c59e5dc74bb665bde1b12c336
Krystian Kopeć <73670385+Cannibal90@users.noreply.github.com>
2021-08-19 22:47:01 +0200

    Add api gateway microservice

---
+++ Backend/microservice-project/gateway-api/src/test/java/com/microservice/booking/gatewayapi/GatewayApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.microservice.booking.gatewayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
