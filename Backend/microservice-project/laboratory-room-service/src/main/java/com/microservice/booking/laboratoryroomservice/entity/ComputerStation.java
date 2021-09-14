1cd539675ec36cc6b3f70473bce25707e9e91924
Krystian Kopeć <73670385+Cannibal90@users.noreply.github.com>
2021-09-15 00:06:50 +0200

    add validation to Laboratory DTO's

--- Backend/microservice-project/laboratory-room-service/src/main/java/com/microservice/booking/laboratoryroomservice/entity/ComputerStation.java
+++ Backend/microservice-project/laboratory-room-service/src/main/java/com/microservice/booking/laboratoryroomservice/entity/ComputerStation.java
@@ -1,6 +1,11 @@
 package com.microservice.booking.laboratoryroomservice.entity;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import com.microservice.booking.laboratoryroomservice.entity.domain.NetworkType;
 import lombok.*;
 
@@ -15,14 +20,38 @@ public class ComputerStation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Min(1)
     private int monitors;
+
+    @NotNull
     private boolean headphones;
+
+    @NotNull
     private boolean microphone;
+
+    @NotBlank
+    @Size(min = 3, max = 25)
     private String operatingSystem;
+
+    @NotBlank
+    @Size(min = 3, max = 25)
     private String graphicCard;
+
+    @NotBlank
+    @Size(min = 3, max = 25)
     private String cpu;
+
+    @Min(2)
+    @NotNull
     private int ram;
+
+    @Min(256)
+    @NotNull
     private int drive;
+
     @Enumerated(value = EnumType.STRING)
+    @NotNull
     private NetworkType networkType;
 }
--- Backend/microservice-project/laboratory-room-service/src/main/java/com/microservice/booking/laboratoryroomservice/entity/Desk.java
+++ Backend/microservice-project/laboratory-room-service/src/main/java/com/microservice/booking/laboratoryroomservice/entity/Desk.java
@@ -5,6 +5,7 @@ import com.microservice.booking.laboratoryroomservice.entity.domain.DeskType;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,9 +19,11 @@ public class Desk {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Enumerated(value = EnumType.STRING)
+    @NotNull
     private DeskType deskType;
-    //ILE PC
+
     @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     @JoinColumn(name="desk_ID")
     private List<ComputerStation> computerStations = new ArrayList<>();
